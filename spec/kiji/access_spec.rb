fbe88551d4d6f84a29f83a9e4e5cf7cf8f7d11dd
kakipo <kakipo@gmail.com>
2015-04-22 20:28:40 +0900

    Create a spec for #withdraw

--- spec/kiji/access_spec.rb
+++ spec/kiji/access_spec.rb
@@ -26,7 +26,7 @@ describe Kiji::Access do
     my_client
   }
 
-  shared_examples_for 'call the API w/ valid parameter' do
+  shared_examples_for 'call the API w/ VALID parameter' do
     it 'should return valid response' do
       method_name = RSpec.current_example.metadata[:example_group][:parent_example_group][:description]
       File.write("tmp/responses/response_#{method_name}.txt", response.body)
@@ -36,81 +36,116 @@ describe Kiji::Access do
       code = xml.at_xpath('//Code').text
       expect(code).to eq '0'
 
-      expect(response.status).to eq valid_status_code
+      expect(response.status).to eq expected_status_code
+    end
+  end
+
+  shared_examples_for 'call the API w/ INVALID parameter' do
+    it 'should return valid response' do
+      method_name = RSpec.current_example.metadata[:example_group][:parent_example_group][:description]
+      File.write("tmp/responses/response_#{method_name}_invalid.txt", response.body)
+
+      xml = Nokogiri::XML(response.body)
+
+      code = xml.at_xpath('//Code').text
+      expect(code).to eq '1'
+
+      expect(response.status).to eq expected_status_code
     end
   end
 
   describe '#apply', :vcr do
-    let(:valid_status_code) { 202 }
+    let(:expected_status_code) { 202 }
     let(:response) do
       file_name = 'apply.zip'
-      file_data = Base64.encode64(File.new('spec/fixtures/apply.zip').read)
+      file_data = Base64.encode64(File.new('spec/fixtures/bulk_apply_for_api.zip').read)
       my_client_with_access_key.apply(file_name, file_data)
     end
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 
   describe '#sended_applications_by_id', :vcr do
-    let(:valid_status_code) { 200 }
+    let(:expected_status_code) { 200 }
     let(:response) { my_client_with_access_key.sended_applications_by_id('201503090951504109') }
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 
   describe '#sended_applications_by_date', :vcr do
-    let(:valid_status_code) { 200 }
+    let(:expected_status_code) { 200 }
     let(:response) { my_client_with_access_key.sended_applications_by_date('20150301', '20150310') }
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 
   describe '#arrived_applications', :vcr do
-    let(:valid_status_code) { 200 }
+    let(:expected_status_code) { 200 }
     let(:response) { my_client_with_access_key.arrived_applications('201503090951504109') }
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 
   describe '#reference', :vcr do
-    let(:valid_status_code) { 200 }
+    let(:expected_status_code) { 200 }
     let(:response) { my_client_with_access_key.reference('9002015000243941') }
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 
   describe '#withdraw' do
-    before do
-      # file_name = 'apply.zip'
-      # file_data = Base64.encode64(File.new('spec/fixtures/apply.zip').read)
-      # apply_response = my_client_with_access_key.apply(file_name, file_data)
-      # apply_xml = Nokogiri::XML(apply_response.body)
-      # @send_number = apply_xml.at_xpath('//SendNumber').text
-      @send_number = '201504131753435049'
+    context 'when INVALID arrive_id is specified', vcr: { re_record_interval: nil } do
+      let(:expected_status_code) { 400 }
+      let(:response) {
+        file_data = Base64.encode64(File.new('spec/fixtures/bulk_apply_for_api.zip').read)
+        my_client_with_access_key.withdraw('9002015000246401', file_data)
+      }
+      it 'returns errors' do
+      end
+      it_behaves_like 'call the API w/ INVALID parameter'
     end
-    it 'test....' do
-      response1 = my_client_with_access_key.sended_applications_by_id(@send_number)
-      File.write('tmp/response_test1.txt', response1.body)
-      xml = Nokogiri::XML(response1.body)
-      error_file = xml.at_xpath('//ErrorFile').text
-      File.write('tmp/test_error_file.html', Base64.decode64(error_file))
-
-      file_data = Base64.encode64(File.new('spec/fixtures/apply.zip').read)
-      my_client_with_access_key.withdraw('', file_data)
+    context 'when VALID arrive_id is specified', vcr: { re_record_interval: 0 } do
+      let(:expected_status_code) { 200 }
+      let(:response) {
+        file_data = Base64.encode64(File.new('spec/fixtures/bulk_apply_for_api.zip').read)
+        my_client_with_access_key.withdraw('9002015000246402', file_data)
+      }
+      it 'returns errors' do
+      end
+      it_behaves_like 'call the API w/ VALID parameter'
     end
+    # before do
+    #   # file_name = 'bulk_apply_for_api.zip'
+    #   # file_data = Base64.encode64(File.new('tmp/bulk_apply_for_api.zip').read)
+    #   # apply_response = my_client_with_access_key.apply(file_name, file_data)
+    #   # File.write('tmp/response_apply_response.txt', apply_response.body)
+    #   # apply_xml = Nokogiri::XML(apply_response.body)
+    #   # @send_number = apply_xml.at_xpath('//SendNumber').text
+    #   @send_number = '201504221845475437'
+    # end
+    # it 'test....' do
+    #   response1 = my_client_with_access_key.sended_applications_by_id(@send_number)
+    #   File.write('tmp/response_withdraw.txt', response1.body)
+    #   xml = Nokogiri::XML(response1.body)
+    #   error_file = xml.at_xpath('//ErrorFile').text
+    #   File.write('tmp/response_withdraw_error_file.html', Base64.decode64(error_file))
+    #
+    #   file_data = Base64.encode64(File.new('spec/fixtures/apply.zip').read)
+    #   my_client_with_access_key.withdraw('', file_data)
+    # end
   end
 
   describe '#amends', :vcr do
-    let(:valid_status_code) { 200 }
+    let(:expected_status_code) { 200 }
     let(:response) { my_client_with_access_key.amends('9002015000243941') }
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 
   describe '#notices', :vcr do
-    let(:valid_status_code) { 200 }
+    let(:expected_status_code) { 200 }
     let(:response) { my_client_with_access_key.notices('9002015000243928') }
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 
   describe '#officialdocument', :vcr do
-    let(:valid_status_code) { 200 }
+    let(:expected_status_code) { 200 }
     let(:response) { my_client_with_access_key.officialdocument('9002015000243931', '1') }
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 
   describe '#verify_officialdocument', :vcr do
@@ -127,26 +162,26 @@ describe Kiji::Access do
         zip_file.to_s.end_with? '.xml'
       }.to_s
     end
-    let(:valid_status_code) { 200 }
+    let(:expected_status_code) { 200 }
     let(:response) { my_client_with_access_key.verify_officialdocument(@arrive_id, @file_name, @file_data, @sig_xml_file_name) }
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 
   describe '#comment', :vcr do
-    let(:valid_status_code) { 200 }
+    let(:expected_status_code) { 200 }
     let(:response) { my_client_with_access_key.comment('9002015000243928', '1') }
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 
   describe '#banks', :vcr do
-    let(:valid_status_code) { 200 }
+    let(:expected_status_code) { 200 }
     let(:response) { my_client_with_access_key.banks }
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 
   describe '#payments', :vcr do
-    let(:valid_status_code) { 200 }
+    let(:expected_status_code) { 200 }
     let(:response) { my_client_with_access_key.payments('9002015000243934') }
-    it_behaves_like 'call the API w/ valid parameter'
+    it_behaves_like 'call the API w/ VALID parameter'
   end
 end
