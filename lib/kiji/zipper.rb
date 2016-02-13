107d97fd4fdefe1547fe9774974214168d66eae2
kakipo <kakipo@gmail.com>
2016-02-13 18:22:06 +0900

    Accept file contents at #sign method

--- lib/kiji/zipper.rb
+++ lib/kiji/zipper.rb
@@ -7,10 +7,16 @@ module Kiji
     end
 
     # 構成管理ファイル（kouse.xml）に署名を施す
-    def sign(kousei_base_file_path, app_file_paths)
+    def sign(kousei_base_file_path_or_content, app_file_paths)
       fail 'Please specify cert & private_key' if @cert.nil? || @private_key.nil?
 
-      kousei_data = Nokogiri::XML(File.read(kousei_base_file_path))
+      content = begin
+                  File.read(kousei_base_file_path_or_content)
+                rescue Errno::ENOENT, Errno::ENAMETOOLONG
+                  kousei_base_file_path_or_content
+                end
+
+      kousei_data = Nokogiri::XML(content)
       kousei_doc = kousei_data.to_xml(save_with:  0)
 
       signer = Signer.new(kousei_doc) do |s|
@@ -42,6 +48,47 @@ module Kiji
       kousei_node.add_next_sibling(signature_node)
 
       signer
+    end
+
+    def write_zip(input_dir, output_file)
+      @input_dir = input_dir
+      @output_file = output_file
+
+      entries = Dir.entries(@input_dir)
+      entries.delete('.DS_Store')
+      entries.delete('.')
+      entries.delete('..')
+      Zip.sort_entries = true
+      Zip::File.open(output_file, Zip::File::CREATE) do |io|
+        write_entries(entries, '', io)
+      end
+    end
+
+    private
+
+    def write_entries(entries, path, io)
+      entries.each do |e|
+        zip_file_path = path == '' ? e : File.join(path, e)
+        disk_file_path = File.join(@input_dir, zip_file_path)
+        # puts 'Deflating ' + disk_file_path
+        if File.directory? disk_file_path
+          recursively_deflate_directory(disk_file_path, io, zip_file_path)
+        else
+          put_into_archive(disk_file_path, io, zip_file_path)
+        end
+      end
+    end
+
+    def recursively_deflate_directory(disk_file_path, io, zip_file_path)
+      # io.mkdir(zip_file_path)
+      subdir = Dir.entries(disk_file_path) - %w(. .. .DS_Store)
+      write_entries(subdir, zip_file_path, io)
+    end
+
+    def put_into_archive(disk_file_path, io, zip_file_path)
+      io.get_output_stream(zip_file_path) do |f|
+        f.puts(File.open(disk_file_path, 'rb').read)
+      end
     end
   end
 end
